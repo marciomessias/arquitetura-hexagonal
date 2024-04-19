4be322f5d01556fb8629b1877d70c522af932c2a
Marcio Messias <marciomessias.developer@gmail.com>
2024-04-19 20:32:47 -0300

    implementado webserver

---
+++ adapters/web/server/server.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/marciomessias/arquitetura-hexagonal/adapters/web/handler"
+	"github.com/marciomessias/arquitetura-hexagonal/application"
+)
+
+type WebServer struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebServer() *WebServer {
+	return &WebServer{}
+}
+
+func (w WebServer) Serve() {
+
+	r := mux.NewRouter()
+	n := negroni.New(
+		negroni.NewLogger(),
+	)
+
+	handler.MakeProductHandlers(r, n, w.Service)
+	http.Handle("/", r)
+
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		Addr:              ":9000",
+		Handler:           nil,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
